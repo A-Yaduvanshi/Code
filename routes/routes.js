5f196563926a74b950ca7a8be62a610560a8aea7
A-Yaduvanshi <amanyadav809@gmail.com>
2022-10-01 23:29:58 +0530

    second commit

--- routes/routes.js
+++ routes/routes.js
@@ -125,11 +125,11 @@ router.get('/jobs',(req,res)=>{
     var desc=req.query.desc;
     var price=req.query.price;
     var hour=req.query.hour;
-    var userid=session.userid;
+    // var userid=session.userid;
     session=req.session;
     if(session.userid){
     if(title != undefined && desc != undefined && price != undefined && hour != undefined){
-        con.query("INSERT INTO `price`(`id`, `title`, `description`, `price`, `hour`,`userid`) VALUES (NULL,'"+title+"','"+desc+"','"+price+"','"+hour+"','"+userid+"')", function (err, result){
+        con.query("INSERT INTO `price`(`id`, `title`, `description`, `price`, `hour`,`userid`) VALUES (NULL,'"+title+"','"+desc+"','"+price+"','"+hour+"','"+session.userid+"')", function (err, result){
             // var data = "{'status':Registration Complete'}";
             res.send({"status":"200",
                 "title":title,"desc":desc,"price":price,"hour":hour}); 
