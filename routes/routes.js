77a4a43ed061523547ee643e08dcb069925ac07b
A-Yaduvanshi <amanyadav809@gmail.com>
2022-10-01 20:53:06 +0530

    first commit

--- routes/routes.js
+++ routes/routes.js
@@ -63,7 +63,7 @@ var session;
 app.get('/', (req, res) => {
     // res.send("hello")
     session=req.session;
-    if(session.userid){
+    if(session){
         res.send("Welcome User <a href=\'/logout'>click to logout</a>");
     }else
     res.send('session not define')
@@ -88,7 +88,7 @@ if(email != undefined && password != undefined){
             //   const comparision =  bcrypt.compare(password, results[0].password)
               if(email==results[0].email&&password==results[0].password){
                 session=req.session;
-                session.id=req.query.email;
+                session=req.query.email;
                res.redirect('/');
                 //   res.send({
                 //     "code":200,
