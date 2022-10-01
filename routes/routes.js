9d77056739538d69894323a65d3c941bc88770c0
A-Yaduvanshi <amanyadav809@gmail.com>
2022-10-01 18:08:45 +0530

    first commit

--- routes/routes.js
+++ routes/routes.js
@@ -105,7 +105,7 @@ var upload = multer({
 
    
  // handle single file upload
- router.post("/post",  (req, res) => {
+ router.post("/post", upload.single('image'), (req, res) => {
     if (!req.file) {
         res.send("No file upload");
     } else {
