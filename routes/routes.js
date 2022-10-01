4df95f76e3eef6a03807fff784af13dfc1c0a258
A-Yaduvanshi <amanyadav809@gmail.com>
2022-10-01 20:28:36 +0530

    first commit

--- routes/routes.js
+++ routes/routes.js
@@ -138,7 +138,7 @@ router.get('/jobs_fetch',(req,res)=>{
         con.query("SELECT * FROM `price`", function (err, result){
             // var data = "{'status':Registration Complete'}";
             
-            res.json(result);
+            res.jsonp(result);
                 // "title":result.title,"desc":result.desc,"price":result.price,"hour":result.hour}); 
         });
 });
