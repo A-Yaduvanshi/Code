6a4e7748112359e4b6a1a003d4e93fd02df8513e
A-Yaduvanshi <amanyadav809@gmail.com>
2022-10-01 13:43:05 +0530

    database details changes

--- mySqlConnect.js
+++ mySqlConnect.js
@@ -1,11 +1,11 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-host: 'localhost',
-// port: 3307,
-database: 'women',
-user: 'root',
-password: ''
+host: 'blvfalkq32ox7e9ivlxb-mysql.services.clever-cloud.com',
+port: 3306,
+database: 'blvfalkq32ox7e9ivlxb',
+user: 'u1hjztgmfr52fobc',
+password: 'GFoAXcn6BX4BmyfRbgDz'
 });
 // local api
 
